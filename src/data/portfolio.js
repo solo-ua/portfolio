51c58b55779da4839402167db1e729d26469fe40
Maria <mariahershi@gmail.com>
2026-03-16 20:24:40 +0200

    Setup GitHub Actions deploy

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,319 +1,319 @@
-export const BASE = '/MariiaKhiershi'
+export const BASE = '/portfolio/'
 
 export const items = [
   {
     "id": 1,
     "cat": "3D Renders",
     "subCat": "Atmospheric",
-    "src": "/MariiaKhiershi/assets/3drenders/atmospheric/1.webp",
+    "src": "/portfolio/assets/3drenders/atmospheric/1.webp",
     "alt": "3D Renders Atmospheric 1"
   },
   {
     "id": 2,
     "cat": "3D Renders",
     "subCat": "Atmospheric",
-    "src": "/MariiaKhiershi/assets/3drenders/atmospheric/2.webp",
+    "src": "/portfolio/assets/3drenders/atmospheric/2.webp",
     "alt": "3D Renders Atmospheric 2"
   },
   {
     "id": 3,
     "cat": "3D Renders",
     "subCat": "Atmospheric",
-    "src": "/MariiaKhiershi/assets/3drenders/atmospheric/3.webp",
+    "src": "/portfolio/assets/3drenders/atmospheric/3.webp",
     "alt": "3D Renders Atmospheric 3"
   },
   {
     "id": 4,
     "cat": "3D Renders",
     "subCat": "Characters",
-    "src": "/MariiaKhiershi/assets/3drenders/characters/1.webp",
+    "src": "/portfolio/assets/3drenders/characters/1.webp",
     "alt": "3D Renders Characters 1"
   },
   {
     "id": 5,
     "cat": "3D Renders",
     "subCat": "Characters",
-    "src": "/MariiaKhiershi/assets/3drenders/characters/2.webp",
+    "src": "/portfolio/assets/3drenders/characters/2.webp",
     "alt": "3D Renders Characters 2"
   },
   {
     "id": 6,
     "cat": "3D Renders",
     "subCat": "Characters",
-    "src": "/MariiaKhiershi/assets/3drenders/characters/3.webp",
+    "src": "/portfolio/assets/3drenders/characters/3.webp",
     "alt": "3D Renders Characters 3"
   },
   {
     "id": 7,
     "cat": "3D Renders",
     "subCat": "Characters",
-    "src": "/MariiaKhiershi/assets/3drenders/characters/4.webp",
+    "src": "/portfolio/assets/3drenders/characters/4.webp",
     "alt": "3D Renders Characters 4"
   },
   {
     "id": 8,
     "cat": "3D Renders",
     "subCat": "Characters",
-    "src": "/MariiaKhiershi/assets/3drenders/characters/5.webp",
+    "src": "/portfolio/assets/3drenders/characters/5.webp",
     "alt": "3D Renders Characters 5"
   },
   {
     "id": 9,
     "cat": "3D Renders",
     "subCat": "Products",
-    "src": "/MariiaKhiershi/assets/3drenders/products/1.webp",
+    "src": "/portfolio/assets/3drenders/products/1.webp",
     "alt": "3D Renders Products 1"
   },
   {
     "id": 10,
     "cat": "3D Renders",
     "subCat": "Products",
-    "src": "/MariiaKhiershi/assets/3drenders/products/2.webp",
+    "src": "/portfolio/assets/3drenders/products/2.webp",
     "alt": "3D Renders Products 2"
   },
   {
     "id": 11,
     "cat": "3D Renders",
     "subCat": "Products",
-    "src": "/MariiaKhiershi/assets/3drenders/products/3.webp",
+    "src": "/portfolio/assets/3drenders/products/3.webp",
     "alt": "3D Renders Products 3"
   },
   {
     "id": 12,
     "cat": "3D Renders",
     "subCat": "Products",
-    "src": "/MariiaKhiershi/assets/3drenders/products/4.webp",
+    "src": "/portfolio/assets/3drenders/products/4.webp",
     "alt": "3D Renders Products 4"
   },
   {
     "id": 13,
     "cat": "Illustrations",
     "subCat": "Fanarts",
-    "src": "/MariiaKhiershi/assets/illustrations/fanarts/1.webp",
+    "src": "/portfolio/assets/illustrations/fanarts/1.webp",
     "alt": "Illustrations Fanarts 1"
   },
   {
     "id": 14,
     "cat": "Illustrations",
     "subCat": "Fanarts",
-    "src": "/MariiaKhiershi/assets/illustrations/fanarts/2.webp",
+    "src": "/portfolio/assets/illustrations/fanarts/2.webp",
     "alt": "Illustrations Fanarts 2"
   },
   {
     "id": 15,
     "cat": "Illustrations",
     "subCat": "Fanarts",
-    "src": "/MariiaKhiershi/assets/illustrations/fanarts/3.webp",
+    "src": "/portfolio/assets/illustrations/fanarts/3.webp",
     "alt": "Illustrations Fanarts 3"
   },
   {
     "id": 16,
     "cat": "Illustrations",
     "subCat": "Fanarts",
-    "src": "/MariiaKhiershi/assets/illustrations/fanarts/4.webp",
+    "src": "/portfolio/assets/illustrations/fanarts/4.webp",
     "alt": "Illustrations Fanarts 4"
   },
   {
     "id": 17,
     "cat": "Illustrations",
     "subCat": "Fanarts",
-    "src": "/MariiaKhiershi/assets/illustrations/fanarts/5.webp",
+    "src": "/portfolio/assets/illustrations/fanarts/5.webp",
     "alt": "Illustrations Fanarts 5"
   },
   {
     "id": 18,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/1.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/1.webp",
     "alt": "Illustrations Illustrations 1"
   },
   {
     "id": 19,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/2.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/2.webp",
     "alt": "Illustrations Illustrations 2"
   },
   {
     "id": 20,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/3.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/3.webp",
     "alt": "Illustrations Illustrations 3"
   },
   {
     "id": 21,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/4.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/4.webp",
     "alt": "Illustrations Illustrations 4"
   },
   {
     "id": 22,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/5.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/5.webp",
     "alt": "Illustrations Illustrations 5"
   },
   {
     "id": 23,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/6.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/6.webp",
     "alt": "Illustrations Illustrations 6"
   },
   {
     "id": 24,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/7.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/7.webp",
     "alt": "Illustrations Illustrations 7"
   },
   {
     "id": 25,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/8.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/8.webp",
     "alt": "Illustrations Illustrations 8"
   },
   {
     "id": 26,
     "cat": "Illustrations",
     "subCat": "Illustrations",
-    "src": "/MariiaKhiershi/assets/illustrations/illustrations/9.webp",
+    "src": "/portfolio/assets/illustrations/illustrations/9.webp",
     "alt": "Illustrations Illustrations 9"
   },
   {
     "id": 27,
     "cat": "Illustrations",
     "subCat": "Portraits",
-    "src": "/MariiaKhiershi/assets/illustrations/portraits/1.webp",
+    "src": "/portfolio/assets/illustrations/portraits/1.webp",
     "alt": "Illustrations Portraits 1"
   },
   {
     "id": 28,
     "cat": "Illustrations",
     "subCat": "Portraits",
-    "src": "/MariiaKhiershi/assets/illustrations/portraits/2.webp",
+    "src": "/portfolio/assets/illustrations/portraits/2.webp",
     "alt": "Illustrations Portraits 2"
   },
   {
     "id": 29,
     "cat": "Illustrations",
     "subCat": "Portraits",
-    "src": "/MariiaKhiershi/assets/illustrations/portraits/3.webp",
+    "src": "/portfolio/assets/illustrations/portraits/3.webp",
     "alt": "Illustrations Portraits 3"
   },
   {
     "id": 30,
     "cat": "Illustrations",
     "subCat": "Portraits",
-    "src": "/MariiaKhiershi/assets/illustrations/portraits/4.webp",
+    "src": "/portfolio/assets/illustrations/portraits/4.webp",
     "alt": "Illustrations Portraits 4"
   },
   {
     "id": 31,
     "cat": "Logos",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/logos/2.webp",
+    "src": "/portfolio/assets/logos/2.webp",
     "alt": "Logos  2"
   },
   {
     "id": 32,
     "cat": "Logos",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/logos/logo.webp",
+    "src": "/portfolio/assets/logos/logo.webp",
     "alt": "Logos  logo"
   },
   {
     "id": 33,
     "cat": "Posters",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/posters/1.webp",
+    "src": "/portfolio/assets/posters/1.webp",
     "alt": "Posters  1"
   },
   {
     "id": 34,
     "cat": "Posters",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/posters/2.webp",
+    "src": "/portfolio/assets/posters/2.webp",
     "alt": "Posters  2"
   },
   {
     "id": 35,
     "cat": "Posters",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/posters/3.webp",
+    "src": "/portfolio/assets/posters/3.webp",
     "alt": "Posters  3"
   },
   {
     "id": 36,
     "cat": "Posters",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/posters/4.webp",
+    "src": "/portfolio/assets/posters/4.webp",
     "alt": "Posters  4"
   },
   {
     "id": 37,
     "cat": "Social Media",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/socialmedia/1.webp",
+    "src": "/portfolio/assets/socialmedia/1.webp",
     "alt": "Social Media  1"
   },
   {
     "id": 38,
     "cat": "Social Media",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/socialmedia/2.webp",
+    "src": "/portfolio/assets/socialmedia/2.webp",
     "alt": "Social Media  2"
   },
   {
     "id": 39,
     "cat": "Social Media",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/socialmedia/3.webp",
+    "src": "/portfolio/assets/socialmedia/3.webp",
     "alt": "Social Media  3"
   },
   {
     "id": 40,
     "cat": "Social Media",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/socialmedia/4.webp",
+    "src": "/portfolio/assets/socialmedia/4.webp",
     "alt": "Social Media  4"
   },
   {
     "id": 41,
     "cat": "Social Media",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/socialmedia/5.webp",
+    "src": "/portfolio/assets/socialmedia/5.webp",
     "alt": "Social Media  5"
   },
   {
     "id": 42,
     "cat": "Social Media",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/socialmedia/6.webp",
+    "src": "/portfolio/assets/socialmedia/6.webp",
     "alt": "Social Media  6"
   },
   {
     "id": 43,
     "cat": "T-shirt Prints",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/tshirtprints/Group 1.webp",
+    "src": "/portfolio/assets/tshirtprints/Group 1.webp",
     "alt": "T-shirt Prints  Group 1"
   },
   {
     "id": 44,
     "cat": "UI Design",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/uidesign/Annual.webp",
+    "src": "/portfolio/assets/uidesign/Annual.webp",
     "alt": "UI Design  Annual"
   },
   {
     "id": 45,
     "cat": "UI Design",
     "subCat": "General",
-    "src": "/MariiaKhiershi/assets/uidesign/Homepage.webp",
+    "src": "/portfolio/assets/uidesign/Homepage.webp",
     "alt": "UI Design  Homepage"
   }
 ]
